be37c0df93bbd31730c7be4ea858c24d79f9ceb9
Shrey K <srkkadia@gmail.com>
2023-09-08 15:11:23 +0530

    Intro

---
+++ Intro/script.js
@@ -0,0 +1,33 @@
+/*
+Data Types:
+> Primitive Data Types
+1. Number -> 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+2. String -> "Hello World"
+3. Boolean -> true, false
+
+> Non-Primitive Data Types
+4. Undefined -> undefined
+5. Null -> null
+6. Symbol -> Symbol()
+
+> Complex Data Types
+7. Array -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+8. Object -> {key: value}
+
+Types of variables in Javascript:
+
+1. var -> it is a global variable, it can be accessed anywhere in the program
+2. let -> it is a local variable, it can be accessed only in the block it is defined
+3. const -> it is a local variable, it can be accessed only in the block it is defined, it cannot be changed
+
+
+Example of hoisting is given below:
+
+console.log(a); // undefined
+// var a = 10;
+
+*/
+
+// console.log(a); // undefined (hoisting)
+// var a = 10;
+// console.log(a);
